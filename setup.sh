24fa58f331bea0d228408791a4fb1f91a5fb7bf8
Balázs Hasprai <balazs.hasprai@hbalazs.com>
2025-06-17 14:32:11 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+_pwd=$(pwd)
+
+cp -f ${_pwd}/zsh/.oh-my-zsh-plugins.gitignore ${_pwd}/zsh/.oh-my-zsh/plugins/.gitignore
+cp -f ${_pwd}/zsh/.oh-my-zsh-themes.gitignore ${_pwd}/zsh/.oh-my-zsh/themes/.gitignore
+
+stow zsh
+stow nvim
+stow tmux
+#stow sway
+
+# Helper function for safe linking
+safe_link() {
+  local src="$1"
+  local dest="$2"
+
+  if [ -L "$dest" ] || [ -e "$dest" ]; then
+    echo "Replacing existing link or file: $dest"
+    rm -rf "$dest"
+  fi
+
+  ln -s "$src" "$dest"
+}
+
+safe_link ~/.zsh/themes/dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+safe_link ~/.zsh/plugins/fzf-tab ~/.oh-my-zsh/plugins/fzf-tab
+safe_link ~/.zsh/plugins/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+safe_link ~/.zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
