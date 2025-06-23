688aa48b82a78e67f7ccc0b6ef6e795f79159a3e
Balázs Hasprai <balazs.hasprai@hbalazs.com>
2025-06-23 17:51:11 +0200

    dtsway-temp

---
+++ dtsway/waybar/custom_modules/kubernetes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export KUBECONFIG="/home/balazshasprai/.kube/r730xd:/home/balazshasprai/.kube/config:/home/balazshasprai/.kube/pix3:/home/balazshasprai/.kube/talos-test"
+
+kubectl config current-context
+
