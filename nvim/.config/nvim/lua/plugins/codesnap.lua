dbcf92b478f6f7d48fe73eb8b76ecfef3305ccde
Balázs Hasprai <balazs.hasprai@hbalazs.com>
2025-07-30 00:54:22 +0200

    nvim: codesnap

---
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,9 @@
+return {
+    { "mistricky/codesnap.nvim", build = "make",
+        opts = {
+            has_breadcrumbs = true,
+            has_line_number = true,
+            bg_padding = 0
+        }
+    }, 
+}
