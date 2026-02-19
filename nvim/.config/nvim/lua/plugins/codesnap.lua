8dbfaa0bb0631ade77625b6812b38821a9051f5a
Balázs Hasprai <balazs.hasprai@hbalazs.com>
2026-02-19 13:29:50 +0100

    fix codesnap on mac by lazy-loading it
    Plugins will be lazy-loaded when one of the following is true:
      - The plugin only exists as a dependency in your spec
      - It has an event, cmd, ft or keys key
      - config.defaults.lazy == true

--- nvim/.config/nvim/lua/plugins/codesnap.lua
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -1,10 +1,11 @@
 return {
-    {}
-    -- { "mistricky/codesnap.nvim", build = "make",
-    --     opts = {
-    --         has_breadcrumbs = true,
-    --         has_line_number = true,
-    --         bg_padding = 0
-    --     }
-    -- }, 
+    -- {}
+    { "mistricky/codesnap.nvim", tag = "v2.0.0",
+        cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapASCII" },
+        opts = {
+            has_breadcrumbs = true,
+            has_line_number = true,
+            bg_padding = 0
+        }
+    },
 }
